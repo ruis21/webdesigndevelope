f44f35edc49e6322860ab72870a510f9e8758024
ruis21 <ruinoa21@gmail.com>
2025-10-01 23:50:06 +0900

    -

--- F-3(연습)/js/script2.js
+++ F-3(연습)/js/script2.js
@@ -1,1 +1,48 @@
-$(".tab-menu li").on("")
+$(function() {
+    $(".menu").on("mouseenter", function(){
+        // $(".sub-menu-list").stop().slideDown();
+        // $(".menu-bg").stop().slideDown();
+        $(".sub-menu-list, .menu-bg").stop().slideDown();
+    });
+    $(".menu").on("mouseleave", function(){
+        // $(".sub-menu-list").stop().slideUp();
+        // $(".menu-bg").stop().slideUp();
+        $(".sub-menu-list, .menu-bg").stop().slideUp();
+    });
+
+
+    setInterval(function(){
+        $(".slide-wrap").animate({"margin-left": "-100%"}, function() {
+            $(".slide:first").appendTo(".slide-wrap");
+            $(".slide-wrap").css("margin-left", "0");
+        });
+    }, 3000);
+
+
+    // tab 
+    $(".tab-menu li").on("click", function(){
+        $(".tab-menu li").removeClass("on");
+        $(this).addClass("on");
+
+        let idx = $(this).index();
+        $(".tab-contents > div").hide();
+        $(".tab-contents > div").eq(idx).show();
+        
+        return false;
+    });
+
+
+    // popup 
+    $(".notice-list li:first-child").on("click", function(){
+        $(".popup").show();
+    });
+    $(".close-btn").on("click", function(){
+        $(".popup").hide();
+    });
+});
+
+
+
+
+
+
