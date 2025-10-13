f70e14688371cecf51bd2659d5e97938ca0d3af4
ruis21 <ruinoa21@gmail.com>
2025-10-13 15:10:19 +0900

    -

--- D-2/js/script.js
+++ D-2/js/script.js
@@ -6,28 +6,38 @@ $(function () {
         $(this).find(".sub-menu-list").stop().slideUp();
     });
 
+
+    // let w = $(window).width() - 430;
     // setInterval(function () {
-    //     if (i < 2) {
-    //         i++;
-    //     } else {
-    //         i = 0;
-    //     }
-    //     $(".slide-wrap").animate({ "margin-left": "-w*i" })
-    //     $(".slide-wrap").animate({ "margin-left": "-w*i" })
+    //     $(".slide-wrap").animate(
+    //         { "margin-left": -w + "px" },
+    //         function () {
+    //             $(".slide:first").appendTo(".slide-wrap");
+    //             $(".slide-wrap").css("margin-left", "0");
+    //         }
+    //     );
+    // }, 3000);
 
-    // });
-
-
+    // slide
     let w = $(window).width() - 430;
+    let i = 0;
     setInterval(function () {
-        $(".slide-wrap").animate(
-            { "margin-left": -w + "px" },
-            function () {
-                $(".slide:first").appendTo(".slide-wrap");
-                $(".slide-wrap").css("margin-left", "0");
-            }
-        );
+        if (i < 2) {
+            i++;
+        } else {
+            i = 0;
+        }
+        $(".slide-wrap").animate({ "margin-left": -w * i });
     }, 3000);
+
+    $(".notice-list:first").on("click", function () {
+        $(".popup").show();
+    })
+
+    $(".close-btn").on("click", function () {
+        $(".popup").hide();
+    })
+
 
 
 });
