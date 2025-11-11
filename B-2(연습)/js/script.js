dadb2ca94ac4b99dbe0f523bb65694cb09d3a63c
ruis21 <ruinoa21@gmail.com>
2025-11-12 00:07:18 +0900

    -

--- B-2(연습)/js/script.js
+++ B-2(연습)/js/script.js
@@ -1,6 +1,22 @@
 $(".menu").on("mouseenter", function () {
     $(this).find(".sub-menu-list").stop().slideDown();
+    $(".menu-bg").stop().slideDown();
 });
 $(".menu").on("mouseleave", function () {
     $(this).find(".sub-menu-list").stop().slideUp();
+    $(".menu-bg").stop().slideUp();
+});
+
+setInterval(function () {
+    $(".slide-wrap").animate({ "margin-top": "-300px" }, function () {
+        $(".slide:first").appentTo(".slide-wrap")
+        $(".slide-wrap").css("margin-top", "0");
+    });
+}, 3000);
+
+$(".notice-list:first").on("click", function () {
+    $(".popup").show();
+});
+$(".close-btn").on("click", function () {
+    $(".popup").hide();
 });
