cd76bc076cd099b0c8a470d24400b799e5edf114
ruis21 <ruinoa21@gmail.com>
2025-12-01 11:51:34 +0900

    -

---
+++ D-1(연습2)/js/script.js
@@ -0,0 +1,33 @@
+$(function () {
+    $(".menu").on("mouseenter", function () {
+        $(this).find(".sub-menu-list").stop().slideDown();
+    })
+    $(".menu").on("mouseleave", function () {
+        $(this).find(".sub-menu-list").stop().slideUp();
+    });
+
+    setInterval(function () {
+        $(".slide-wrap").animate({ "margin-top": "-400px" }, function () {
+            $(".slide:first").appendTo(".slide-wrap");
+            $(".slide-wrap").css("margin-top", "0");
+        });
+    }, 3000);
+
+
+    $(".tab-menu li").on("click", function () {
+        let idx = $(this).index();
+        $(".tab-menu li").removeClass("on");
+        $(this).addClass("on");
+
+        $(".tab-conts>div").hide();
+        $(".tab-conts>div").eq(idx).show();
+    });
+
+    $(".notice-list li:first").on("click", function () {
+        $(".popup").show();
+    });
+    $(".close-btn").on("click", function () {
+        $(".popup").hide();
+    });
+});
+
