00f4cc697eb991589c82bd4e1ce017dbc0ed9695
ruis21 <ruinoa21@gmail.com>
2025-12-07 00:27:02 +0900

    -

---
+++ C-4(연습)/script/script.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".menu").on("mouseenter", function () {
+        $(this).find(".sub-menu-list").stop().slideDown();
+    });
+    $(".menu").on("mouseleave", function () {
+        $(this).find(".sub-menu-list").stop().slideUp();
+    });
+
+
+    setInterval(function () {
+        $(".slide-wrap").animate({ "margin-left": "-800px" }, function () {
+            $(".slide:first").appendTo(".slide-wrap");
+            $(".slide-wrap").css("margin-left", "0");
+        })
+    }, 3000)
+});
